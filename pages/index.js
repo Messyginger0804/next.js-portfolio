0c218f2d6fb01ba8051cd9a8a3720e520d1cc7b8
JC <j.c.ashley4363@gmail.com>
2023-03-15 21:52:29 -0500

    fied live demo btton for portfolio

--- pages/index.js
+++ pages/index.js
@@ -207,7 +207,7 @@ function Home() {
               <h4 className='py-4 text-sky-200'>A Clone of Google.com using Google's API</h4>
               <div className='flex justify-center gap-5'>
                 <a href='https://github.com/Messyginger0804/gugal.git' className='hover:bg-white p-2 text-sky-400 border-2 border-sky-400' target='_blank'>Github</a>
-                <a href='https://jcsgoogle.netlify.app' className='hover:bg-white p-2 text-sky-400 border-2 border-sky-400' target='_blank'>Live Demo</a>
+                <a className='hover:bg-white p-2 text-sky-400 border-2 border-sky-400 hover:cursor-not-allowed' target='_blank'>Live Demo</a>
               </div>
 
             </div>
@@ -217,7 +217,7 @@ function Home() {
               <h4 className='py-4 text-sky-200'>Developed Using Next.js & Tailwindcss</h4>
               <div className='flex justify-center gap-5'>
                 <a href='https://github.com/Messyginger0804/next.js-portfolio.git' className='hover:bg-white p-2 text-sky-400 border-2 border-sky-400' target='_blank'>Github</a>
-                <a href='https://jcsgoogle.netlify.app' className='hover:bg-white p-2 text-sky-400 border-2 border-sky-400' target='_blank'>Live Demo</a>
+                <a href='https://jcashleyportfolio.netlify.app' className='hover:bg-white p-2 text-sky-400 border-2 border-sky-400' target='_blank'>Live Demo</a>
               </div>
 
             </div>
