71d8f670837f90d010d77e916a286b5be6190e61
JC <j.c.ashley4363@gmail.com>
2023-11-04 09:36:23 -0500

    finished responsive design for home buttons

--- pages/index.js
+++ pages/index.js
@@ -64,10 +64,10 @@ function Home() {
 
             {/* CTA buttons */}
             <div className='flex justify-center gap-2 md:gap-4 text-xs md:text-sm'>
-              <a href='https://drive.google.com/file/d/1rp7fh3JPqZrIEaAeRySZkE4FO82GM1wb/view?usp=sharing' target='_blank' className='hover:bg-white p-2 text-sky-400 border-2 border-sky-400'>
+              <a href='https://drive.google.com/file/d/1rp7fh3JPqZrIEaAeRySZkE4FO82GM1wb/view?usp=sharing' target='_blank' className='hover:bg-white p-1 md:p-2 text-sky-400 border-2 border-sky-400'>
                 Resume</a>
-              <a className='hover:bg-white bg-sky-300 p-2 text-gray-800 border-2 border-sky-400' href="#contact">Contact Me</a>
-              <a className='hover:bg-sky-300 bg-white p-2 text-gray-800 border-2 border-sky-400' target='_blank' href="https://blogsbyjc.vercel.app/">
+              <a className='hover:bg-white bg-sky-300 p-1 md:p-2 text-gray-800 border-2 border-sky-400' href="#contact">Contact Me</a>
+              <a className='hover:bg-sky-300 bg-white p-1 md:p-2 text-gray-800 border-2 border-sky-400' target='_blank' href="https://blogsbyjc.vercel.app/">
                 My Blog</a>
             </div>
 
