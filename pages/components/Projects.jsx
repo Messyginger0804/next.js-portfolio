013a2ac603baa8338eec1af4e89827a8b0295be5
JC <j.c.ashley4363@gmail.com>
2023-12-28 10:43:37 -0600

    perf(Project.jsx): created project.jsx maping over data

---
+++ pages/components/Projects.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+import { projects } from 'data/projects'
+import Image from 'next/image'
+
+function Projects() {
+    return (
+        <div className=' flex justify-center flex-wrap lg:flex gap-5'>
+            {projects.map((proj) => (
+                <div className='text-center p-10 rounded-xl  my-10 max-w-sm shadow-sky-400 shadow-xl hover:shadow-sky-300'>
+
+                    <h3 className='text-lg font-medium pt-8 pb-2'>{proj.title}</h3>
+                    <div className='flex justify-center'>
+                        <Image
+                            className='flex justify-center fit object-center rounded'
+                            src={proj.image}
+                            width={100}
+                            height={100}
+                            alt={proj.title}
+                        />
+                    </div>
+                    <h3 className='text-lg font-medium pt-8 pb-2'>{proj.descTitle}</h3>
+                    <p className='pb-4 text-xs'>{proj.desc}</p>
+                    <div className='flex justify-center gap-5'>
+                        <a href={proj.rep} className='hover:bg-white p-2 text-sky-400 border-2 border-sky-400 rounded-lg' target='_blank'>github</a>
+                        <a href={proj.live} className='hover:bg-white p-2 text-sky-400 border-2 border-sky-400 rounded-lg' target='_blank'>Live Site</a>
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default Projects
