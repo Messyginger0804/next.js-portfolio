dac685f1bd153411d23757d3a543fab9ab4f795a
JC <j.c.ashley4363@gmail.com>
2023-12-29 06:52:15 -0600

    build(AboutMe.jsx): fixed building error

--- pages/components/AboutMe.jsx
+++ pages/components/AboutMe.jsx
@@ -42,3 +42,5 @@ export const AboutMe = () => {
         </>
     )
 }
+
+export default AboutMe;
--- pages/components/TechStack.jsx
+++ pages/components/TechStack.jsx
@@ -16,11 +16,12 @@ function TechStack() {
                 <div className='text-center w-full md:w-1/2 shadow-xl shadow-sky-400 p-10 rounded-xl my-10 hover:shadow-sky-300 cursor-none'>
                     <h3 className='text-lg font-medium pb-2 text-sky-300'>FRONT-END</h3>
                     <div className='flex flex-wrap gap-5 hover:ease-out'>
-                        {frontTech.map((tech) => (
+                        {frontTech.map((tech) =>
+                            // <article className=''>
                             <article className='text-center flex justify-center gap-1'>
                                 <h4>{tech.icon}</h4><h4>{tech.name}</h4>
                             </article>
-                        ))}
+                        )}
                     </div>
                 </div>
                 <div className='text-center p-10 rounded-xl my-10 w-full md:w-1/2 shadow-sky-400 shadow-xl hover:shadow-sky-300 cursor-none'>
