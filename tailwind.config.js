1adb9c68910d8014aa619432ac5300df974d14e4
JC <j.c.ashley4363@gmail.com>
2024-01-01 09:10:11 -0600

    added animation to tech stacks

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'dance': ["Dancing Script"],
+      'nunito': ['nunito', 'sans-serif'],
+      'MyFont': ['"My Font"', 'serif'] // Ensure fonts with spaces have " " surrounding it.
+    },
     extend: {
       transitionTimingFunction: {
         'in-expo': 'cubic-bezier(0.95, 0.05, 0.795, 0.035)',
@@ -14,5 +19,7 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: [
+
+  ],
 }
