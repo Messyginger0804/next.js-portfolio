50990a560721472214dd5b50223eee5d3b7ecb5b
JC <j.c.ashley4363@gmail.com>
2024-01-03 20:30:50 -0600

    perf(utilities.js): fix link for linkedin

--- data/utilities.js
+++ data/utilities.js
@@ -4,7 +4,7 @@ export const link = {
     resume: 'https://drive.google.com/file/d/1rp7fh3JPqZrIEaAeRySZkE4FO82GM1wb/view?usp=drive_link',
 
 
-    linkedin: 'https://github.com/Messyginger0804',
+    linkedin: 'https://www.linkedin.com/in/jeremy-ashley-webdev/',
     github: 'https://github.com/messyginger0804',
     twitter: 'https://twitter.com/messyginger0804',
 
