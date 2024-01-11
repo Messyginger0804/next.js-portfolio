f8b8e864c21430b707275665adc77803f2c1d4db
JC <j.c.ashley4363@gmail.com>
2024-01-11 06:12:10 -0600

    styles(Projects.jsx): moved buttons to the bottom of each project card

--- pages/components/FreeLance.jsx
+++ pages/components/FreeLance.jsx
@@ -12,7 +12,7 @@ function FreeLance() {
             <h3 className='text-center text-3xl py-1 text-sky-300'>Contracted Jobs</h3>
 
             <div className=' flex justify-center flex-wrap lg:flex gap-10'>
-                <div className='text-center p-10 rounded-xl my-10 shadow-sky-400 shadow-xl hover:shadow-sky-300'>
+                <div className='text-center p-10 w-[280px] rounded-xl my-10 shadow-sky-400 shadow-xl hover:shadow-sky-300'>
                     <h3 className='text-lg font-medium pt-8 pb-2'>Top Flight Lawn Service</h3>
                     <p><span className='px-2'>client:</span>Rene Guillen</p>
                     <a href='tel:2147794259' className='hover:underline'>214-779-4259</a>
@@ -29,7 +29,8 @@ function FreeLance() {
                     </Link>
                     <h4 className='py-4 text-sky-200'>DFW Landscaping Company</h4>
                 </div>
-                <div className='text-center p-10 rounded-xl my-10 shadow-sky-400 shadow-xl hover:shadow-sky-300'>
+
+                <div className='text-center p-10 rounded-xl my-10 w-[280px] shadow-sky-400 shadow-xl hover:shadow-sky-300'>
                     <h3 className='text-lg font-medium pt-8 pb-2'>JLD Counseling</h3>
                     <p><span className='px-2'>client:</span>Jason Daughtery</p>
                     <a href='tel:2147794259' className='hover:underline'>214-785-7426</a>
--- pages/components/Projects.jsx
+++ pages/components/Projects.jsx
@@ -8,14 +8,11 @@ import {
 
 function Projects() {
     return (
-        <div className=' flex justify-center flex-wrap lg:flex gap-5'>
+        <div className='flex justify-center flex-wrap lg:flex gap-5'>
             {projects.map((proj) => (
-                <div className='text-center p-10 rounded-xl  my-10 max-w-sm shadow-sky-400 shadow-xl hover:shadow-sky-300'>
-
-                    {
-                        heading(proj.inProgress)
-                    }
-                    <h3 className='text-lg font-medium pt-8 pb-2'>{proj.title}</h3>
+                <div className='text-center p-10 rounded-xl w-[280px] h-[500px] my-6 max-w-sm relative shadow-sky-400 shadow-xl hover:shadow-sky-300'>
+                    {heading(proj.inProgress)}
+                    <h3 className='text-lg font-medium pt-2 pb-2'>{proj.title}</h3>
                     <div className='flex justify-center'>
                         <Image
                             className='flex justify-center fit object-center rounded'
@@ -25,13 +22,13 @@ function Projects() {
                             alt={proj.title}
                         />
                     </div>
-                    <h3 className='text-lg font-medium pt-8 pb-2'>{proj.descTitle}</h3>
+                    <h3 className='text-lg font-medium pt-2 pb-2'>{proj.descTitle}</h3>
                     <p className='pb-4 text-xs'>{proj.desc}</p>
-                    <div className='flex justify-center gap-5'>
+
+                    {/* Buttons container */}
+                    <div className='flex justify-evenly absolute bottom-0 left-0 w-full p-4'>
                         <a href={proj.rep} className='hover:bg-white p-2 text-sky-400 border-2 border-sky-400 rounded-lg' target='_blank'>github</a>
-                        {
-                            button(proj.isPublished, proj.live)
-                        }
+                        {button(proj.isPublished, proj.live)}
                     </div>
                 </div>
             ))}
