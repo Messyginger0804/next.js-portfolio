5af87e7ed3e22d251ed0054cdca5e448aadaaff7
JC <j.c.ashley4363@gmail.com>
2024-01-11 08:16:56 -0600

    style(Navbar.jsx Project.jsx): changed positioning of items to not disapear of when scrolling

--- pages/components/Navbar.jsx
+++ pages/components/Navbar.jsx
@@ -1,23 +1,22 @@
-import React from 'react'
+import React from 'react';
 import { AiOutlineHome, AiOutlineUser, AiTwotoneFolderOpen } from 'react-icons/ai';
 import { MdComputer, MdMessage } from 'react-icons/md';
 
-
 function Navbar() {
     return (
-        <nav class="bg-white bg-opacity-75 rounded-full flex justify-center w-fit border border-white border-solid fixed bottom-0 transform -translate-x-1/2 -translate-y-1/2 left-1/2 p-2">
-            <a href='#' class='text-blue-900 p-2 
-        hover:bg-sky-300 hover:text-blue-900 hover:rounded-full hover:shadow'><AiOutlineHome class='text-4xl' /></a>
-            <a href='#about' class='text-blue-900 p-2 
-        hover:bg-sky-300 hover:text-blue-900 hover:rounded-full hover:shadow'><AiOutlineUser class='text-4xl' /></a>
-            <a href='#tech stack' class='text-blue-900 p-2 
-        hover:bg-sky-300 hover:text-blue-900 hover:rounded-full hover:shadow'><MdComputer class='text-4xl' /></a>
-            <a href='#experience' class='text-blue-900 p-2 
-        hover:bg-sky-300 hover:text-blue-900 hover:rounded-full hover:shadow'><AiTwotoneFolderOpen class='text-4xl' /></a>
-            <a href='#contact' class='text-blue-900 p-2 
-        hover:bg-sky-300 hover:text-blue-900 hover:rounded-full hover:shadow'><MdMessage class='text-4xl' /></a>
+        <nav className="bg-white bg-opacity-75 rounded-full flex justify-center w-fit border border-white border-solid fixed bottom-0 transform -translate-x-1/2 -translate-y-1/2 left-1/2 p-2 z-50">
+            <a href='#' className='text-blue-900 p-2 
+                hover:bg-sky-300 hover:text-blue-900 hover:rounded-full hover:shadow'><AiOutlineHome className='text-4xl' /></a>
+            <a href='#about' className='text-blue-900 p-2 
+                hover:bg-sky-300 hover:text-blue-900 hover:rounded-full hover:shadow'><AiOutlineUser className='text-4xl' /></a>
+            <a href='#tech stack' className='text-blue-900 p-2 
+                hover:bg-sky-300 hover:text-blue-900 hover:rounded-full hover:shadow'><MdComputer className='text-4xl' /></a>
+            <a href='#experience' className='text-blue-900 p-2 
+                hover:bg-sky-300 hover:text-blue-900 hover:rounded-full hover:shadow'><AiTwotoneFolderOpen className='text-4xl' /></a>
+            <a href='#contact' className='text-blue-900 p-2 
+                hover:bg-sky-300 hover:text-blue-900 hover:rounded-full hover:shadow'><MdMessage className='text-4xl' /></a>
         </nav>
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;
--- pages/components/Projects.jsx
+++ pages/components/Projects.jsx
@@ -7,37 +7,39 @@ function Projects() {
     const maxDescLength = 150; // Set your desired maximum length for the project description
 
     return (
-        <div className='flex justify-center flex-wrap lg:flex gap-5 '>
-            {projects.map((proj) => (
-                <div className='text-center p-10 rounded-xl w-[280px] h-[400px] my-6 max-w-sm relative shadow-sky-400 shadow-xl hover:shadow-sky-300 '>
-                    <div className='flex flex-col justify-center h-[400px] relative top-1/2 transform -translate-y-1/2'>
-                        <h3 className='text-lg font-medium'>{proj.title}</h3>
+        <div className='relative z-10'>
+            <div className='flex justify-center flex-wrap lg:flex gap-5'>
+                {projects.map((proj) => (
+                    <div key={proj.title} className='text-center p-10 rounded-xl w-[280px] h-[400px] my-6 max-w-sm relative shadow-sky-400 shadow-xl hover:shadow-sky-300'>
+                        <div className='flex flex-col justify-center h-[400px] relative top-1/2 transform -translate-y-1/2'>
+                            <h3 className='text-lg font-medium'>{proj.title}</h3>
 
-                        <div className='flex justify-center mb-0'>
-                            <Image
-                                className='fit object-center rounded'
-                                src={proj.image}
-                                width={100}
-                                height={100}
-                                alt={proj.title}
-                            />
-                            {heading(proj.inProgress)}
+                            <div className='flex justify-center mb-0'>
+                                <Image
+                                    className='fit object-center rounded'
+                                    src={proj.image}
+                                    width={100}
+                                    height={100}
+                                    alt={proj.title}
+                                />
+                                {heading(proj.inProgress)}
+                            </div>
+
+                            {/* Description container */}
+                            <div className='h-[150px] my-2'>
+                                <h3 className='text-lg font-medium pt-1 pb-1'>{proj.descTitle}</h3>
+                                <p className='text-xs custom-scrollbar'>{truncateText(proj.desc, maxDescLength)}</p>
+                            </div>
                         </div>
 
-                        {/* Description container */}
-                        <div className='h-[150px] my-2'>
-                            <h3 className='text-lg font-medium pt-1 pb-1'>{proj.descTitle}</h3>
-                            <p className='text-xs custom-scrollbar'>{truncateText(proj.desc, maxDescLength)}</p>
+                        {/* Buttons container */}
+                        <div className='flex justify-evenly absolute bottom-0 left-0 w-full p-2 z-20'>
+                            <a href={proj.rep} className='hover:bg-white p-2 text-sky-400 border-2 border-sky-400 rounded-lg' target='_blank'>github</a>
+                            {button(proj.isPublished, proj.live)}
                         </div>
                     </div>
-
-                    {/* Buttons container */}
-                    <div className='flex justify-evenly absolute bottom-0 left-0 w-full p-2'>
-                        <a href={proj.rep} className='hover:bg-white p-2 text-sky-400 border-2 border-sky-400 rounded-lg' target='_blank'>github</a>
-                        {button(proj.isPublished, proj.live)}
-                    </div>
-                </div>
-            ))}
+                ))}
+            </div>
         </div>
     );
 }
