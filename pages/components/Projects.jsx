15782204c67e17399f23e3d930fb46f46f6edb81
JC <j.c.ashley4363@gmail.com>
2024-01-14 08:48:41 -0600

    style(Projects.jsx): imported proper styles from previous commit with flex display

--- pages/components/Projects.jsx
+++ pages/components/Projects.jsx
@@ -4,16 +4,18 @@ import ProjectCard from './ProjectCard';
 
 function Projects({ projects }) {
     return (
-        <>
-            {projects?.map((proj) => (
-                <div className='flex justify-center gap-3'>
-                    <ProjectCard
-                        key={proj.title}
-                        proj={proj}
-                    />
-                </div>
-            ))}
-        </>
+        <div className='relative z-10'>
+            <div className='flex justify-center flex-wrap lg:flex gap-5'>
+                {projects?.map((proj) => (
+                    <div className='flex justify-center gap-3'>
+                        <ProjectCard
+                            key={proj.title}
+                            proj={proj}
+                        />
+                    </div>
+                ))}
+            </div>
+        </div>
     );
 }
 
