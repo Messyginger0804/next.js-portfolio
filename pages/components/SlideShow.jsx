17791bf123a1d54e5bc493174f0203cc2063262c
JC <j.c.ashley4363@gmail.com>
2024-01-14 07:33:48 -0600

    styles(SlideShow.jsx):: got slideshow centered and moving with time

--- pages/components/SlideShow.jsx
+++ pages/components/SlideShow.jsx
@@ -8,16 +8,18 @@ import { projects } from 'data/projects';
 
 function SlideShow() {
     return (
-        <div className="flex justify-center items-center h-screen relative w-fit">
+        <div className="w-3/4 m-auto">
             <Carousel
-                className=''
+                // className='max-[400px]'
                 // height={500}
                 // width={500}
                 NextIcon={<BiArrowToRight className="text-4xl text-white" />}
                 PrevIcon={<BiArrowToLeft className="text-4xl text-white" />}
             >
                 {projects.map((proj) => (
-                    <ProjectCard key={proj.title} project={proj} />
+                    <div className='flex justify-center gap-3'>
+                        <ProjectCard key={proj.title} project={proj} />
+                    </div>
                 ))}
             </Carousel>
         </div>
